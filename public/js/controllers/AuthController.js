fc8df3b0c970012addd40e32cc4264e2110daff9
daniel alroy <danielalroy@daniels-MacBook-Pro.local>
2016-08-23 14:53:18 +0300

    add navbar

--- public/js/controllers/AuthController.js
+++ public/js/controllers/AuthController.js
@@ -1,7 +1,15 @@
 app.controller('AuthCtrl', ['$scope', 'auth', '$state', function($scope, auth, $state){
 
+  //new user register
   $scope.register = function () {
     auth.register($scope.user).then(function(){
+      $state.go('home');
+    });
+  };
+
+  // existing user login
+  $scope.login = function() {
+    auth.login($scope.user).then(function() {
       $state.go('home');
     });
   };
--- public/js/services/auth.js
+++ public/js/services/auth.js
@@ -3,12 +3,13 @@ app.factory('auth', ['$http', function($http){
   var auth = {}; //empty object
 
   auth.register = function(user){
-     return $http.post('/register', user)
+     return $http.post('/register', user);
     };
 
-
-
+  auth.login = function(user) {
+    return $http.post('/login', user);
+  }
 
   return auth;
-  
+
 }]);
