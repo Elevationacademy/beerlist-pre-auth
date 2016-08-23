1d8ca8b7d285dddafa05f05616dcdd22bb5aae27
daniel alroy <danielalroy@daniels-MacBook-Pro.local>
2016-08-23 11:33:34 +0300

    redirect new user to home

--- public/js/controllers/AuthController.js
+++ public/js/controllers/AuthController.js
@@ -1,7 +1,8 @@
-app.controller('AuthCtrl', ['$scope', 'auth', function($scope, auth){
+app.controller('AuthCtrl', ['$scope', 'auth', '$state', function($scope, auth, $state){
 
   $scope.register = function () {
-    auth.register($scope.user);
+    auth.register($scope.user).then(function(){
+      $state.go('home');
+    });
   };
-  
 }]);
